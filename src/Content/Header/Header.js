8258921400f63d910621cda2c30e3fd118346d1f
igorSachkov <igordadadadanet@yandex.ru>
2021-08-22 16:52:39 +0300

    header готов!

--- src/Content/Header/Header.js
+++ src/Content/Header/Header.js
@@ -11,6 +11,48 @@ import hamburgerButton from "./../../icons/hamburger-button.png"
 import planning from "./../../icons/Planning.png"
 import { Grid } from '@material-ui/core';
 import { useState } from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import Menu from '@material-ui/core/Menu';
+import MenuItem from '@material-ui/core/MenuItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import InboxIcon from '@material-ui/icons/MoveToInbox';
+import DraftsIcon from '@material-ui/icons/Drafts';
+import SendIcon from '@material-ui/icons/Send';
+
+
+const StyledMenu = withStyles({
+  paper: {
+    border: '1px solid #d3d4d5',
+  },
+})((props) => (
+  <Menu
+    elevation={0}
+    getContentAnchorEl={null}
+    anchorOrigin={{
+      vertical: 'bottom',
+      horizontal: 'center',
+    }}
+    transformOrigin={{
+      vertical: 'top',
+      horizontal: 'center',
+    }}
+    {...props}
+  />
+));
+
+const StyledMenuItem = withStyles((theme) => ({
+  root: {
+    '&:focus': {
+      backgroundColor: theme.palette.primary.main,
+      '& .MuiListItemIcon-root, & .MuiListItemText-primary': {
+        color: theme.palette.common.white,
+      },
+    },
+  },
+}))(MenuItem);
+
 
 
 
@@ -36,10 +78,41 @@ export default function Header() {
             edge="start"
             color="inherit"
             aria-label="open drawer"
+            onClick={handleClick}
           >
-            <img src={hamburgerButton} className="hamburger-icon"/>
+            <img src={hamburgerButton} className="hamburger-icon" />
           </IconButton>
-          
+
+          <StyledMenu
+            id="customized-menu"
+            anchorEl={anchorEl}
+            keepMounted
+            open={Boolean(anchorEl)}
+            onClose={handleClose}
+          >
+            <StyledMenuItem>
+              <ListItemIcon>
+                <SendIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Sent mail" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <DraftsIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Drafts" />
+            </StyledMenuItem>
+            <StyledMenuItem>
+              <ListItemIcon>
+                <InboxIcon fontSize="small" />
+              </ListItemIcon>
+              <ListItemText primary="Inbox" />
+            </StyledMenuItem>
+          </StyledMenu>
+
+
+
+
         </Grid>
         <Grid item xs={6}>
           <div className="add-planning" >
