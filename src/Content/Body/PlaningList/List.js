2a94e08ec31997fde47c5b640144e8e7a4b2c45c
igorSachkov <igordadadadanet@yandex.ru>
2021-09-14 21:19:23 +0300

    + адаптив для моб устр

--- src/Content/Body/PlaningList/List.js
+++ src/Content/Body/PlaningList/List.js
@@ -31,47 +31,47 @@ const List = ({ items, deletePlanningItem, editPlanningItem, containerState }) =
         <div >
             <div className={classes.root}>
                 <Grid container spacing={3}>
-                    <Grid item xs={2} md={1}>
+                    <Grid item xs={3} md={1}>
                         Дата начала
                     </Grid>
-                    <Grid item xs={5}>
+                    <Grid item xs={4} md={5}>
                         Название
                     </Grid>
-                    <Grid item xs={2}>
-                        Участников
+                    <Grid item xs={2} md={2} className="members">
+                        
                     </Grid>
-                    <Grid item xs={2} className="coach">
+                    <Grid item xs={0} md={2} className="coach">
                         Тренер
                     </Grid>
-                    <Grid item xs={1}>
+                    <Grid item xs={3} md={1}>
                         Прогресс
                     </Grid>
-                    <Grid item xs={1}></Grid>
+                    <Grid item xs={0} md={1} className="edit-item-column"></Grid>
                 </Grid>
             </div>
             <div className="items-container">
                 {containerState.map((item, i) => {
                     return (<div key={item.id} className="list__item">
                         <Grid container spacing={3} alignItems="center">
-                            <Grid item xs={2} md={1}>
+                            <Grid item xs={3} md={1}>
                                 {showDate(item.date)}
                             </Grid>
-                            <Grid item xs={5}>
+                            <Grid item xs={4} md={5}>
                                 {item.name}
                             </Grid>
-                            <Grid item xs={2}>
+                            <Grid item xs={2} md={2}>
                                 {item.members}
                             </Grid>
-                            <Grid item xs={2} className="coach">
+                            <Grid item xs={0} md={2} className="coach">
                                 {item.coach}
                             </Grid>
-                            <Grid item xs={1}>
+                            <Grid item xs={3} md={1}>
                                 <div className="progress-percents">
                                     <img src={progressIcon} /> {item.progress} %
                                 </div>
 
                             </Grid>
-                            <Grid item xs={1}>
+                            <Grid item xs={0} md={1} className="edit-item-column">
                                 <ListItemIcon targetId={item.id} deletePlanningItem={deletePlanningItem} editPlanningItem={editPlanningItem} item={item} />
                             </Grid>
                         </Grid>
